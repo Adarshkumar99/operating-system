1f2bd1f4c9e7682a153bb707dcc5bd3a24268dbb
Adarshkumar99 <56436370+Adarshkumar99@users.noreply.github.com>
2020-03-21 13:34:42 +0530

    Add files via upload

---
+++ opera.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+int main()
+{
+    int bst[20],p[20],waitt[20],tat[20],pr[20],i,j,n,tot=0,pos,tem,avg_wait,avg_tat;
