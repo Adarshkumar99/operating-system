94f7285a8cd01dc6ba84a7a74d6d7b31659994c1
Adarshkumar99 <56436370+Adarshkumar99@users.noreply.github.com>
2020-03-25 12:35:57 +0530

    Update opera.c

--- opera.c
+++ opera.c
@@ -4,11 +4,11 @@
 #include<sys/stat.h>
 int main()
 {
-  int bst[20],p[20],waitt[20],tat[20],pr[20],i,j,n,tot=0,pos,tem,avg_wait,avg_tat;
+  int bst[20],pro[20],waitt[20],tat[20],pr[20],i,j,n,tot=0,pos,tem,avg_wait,avg_tat;
      printf("Enter total no of process:");
     scanf("%d",&n);
  
-    printf("\nEnter BurstTime and priority\n");
+    printf("\nEnter burst time and priority\n");
     for(i=0;i<n;i++)
     {
         printf("\nP[%d]\n",i+1);
